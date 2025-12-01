62464f219ca8e81f8d24fe220f5a21e744855562
202407042 <minseo5830@yuhan.ac.kr>
2025-12-01 13:54:40 +0900

    processImage.jsp 수정

--- src/main/java/com/oreilly/servlet/MultipartRequest.java
+++ src/main/java/com/oreilly/servlet/MultipartRequest.java
@@ -1,4 +1,13 @@
 package com.oreilly.servlet;
 
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import jakarta.servlet.http.HttpServletRequest;
+
 public class MultipartRequest {
+    public MultipartRequest(HttpServletRequest request, String realPath, int maxSize, String s, DefaultFileRenamePolicy defaultFileRenamePolicy) {
+    }
+
+    public String getParameter(String quoteText) {
+        return null;
+    }
 }
